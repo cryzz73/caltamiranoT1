9efd2a4d5f8d41e3af6215e9d1927a727fea0a35
cryzz <cryzz@CRYZZ>
2024-04-07 23:31:49 -0500

    Agregar archivos de proyecto.

---
+++ caltamiranoT1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace caltamiranoT1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Vistas.vPrincipal();
+        }
+    }
+}
---
+++ caltamiranoT1/Vistas/vPrincipal.xaml.cs
@@ -0,0 +1,21 @@
+namespace caltamiranoT1.Vistas;
+
+public partial class vPrincipal : ContentPage
+{
+	public vPrincipal()
+	{
+		InitializeComponent();
+	}
+
+    private void btnConsulta_Clicked(object sender, EventArgs e)
+    {
+        string nombre = txtNombre.Text;
+        string apellido = txtApellido.Text;
+        string edad = txtEdad.Text;
+        double salario = Convert.ToInt32(txtSalario.Text);
+        double calculo = salario * 0.0945;
+        DisplayAlert("Alerta", "Bienvenido " + nombre  +  apellido + "\n Edad: " +edad+ "años \n Tu aporte mensual al IESS es: \n $" + calculo+ "de manera mensual", "Cerrar");
+ 
+
+    }
+}
